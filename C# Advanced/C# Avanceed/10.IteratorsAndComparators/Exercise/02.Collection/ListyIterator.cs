a573a4ff1a3061fe31edb7244454fc94a829b78b
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-25 16:04:31 +0300

    Added new problem
    Added problem "02.Collection" in solution "10.IteratorsAndComparators"

---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/02.Collection/ListyIterator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Collection
+{
+    class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int index;
+
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+        }
+
+        public bool Move()
+        {
+            if (index + 1 < elements.Count)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < elements.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(elements[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in elements)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/02.Collection/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Collection
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> elements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+
+            ListyIterator<string> listyIterator = new ListyIterator<string>(elements);
+
+            string cmd = Console.ReadLine();
+            while (true)
+            {
+                if (cmd == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (cmd == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (cmd == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception exception)
+                    {
+
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                else if (cmd == "PrintAll")
+                {
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                cmd = Console.ReadLine();
+            }
+        }
+    }
+}
