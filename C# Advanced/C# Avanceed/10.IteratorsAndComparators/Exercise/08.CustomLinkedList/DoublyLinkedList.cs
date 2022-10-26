a12e246f5a37059769f84c8c5119a69fbf8e6500
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-26 23:57:38 +0300

    Added new problem
    Added problem "08.CustomLinkedList" in folder "Exercise"  in "10.IteratorsAndComparators"

---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/08.CustomLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        private class ListNode
+        {
+            public ListNode(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+
+            public ListNode NextNode { get; set; }
+
+            public ListNode PrevNode { get; set; }
+        }
+
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = head;
+                head.PrevNode = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element);
+                newTail.PrevNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = head.Value;
+
+            if (Count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.NextNode;
+                head.PrevNode = null;
+            }
+
+            Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = tail.Value;
+
+            if (Count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.PrevNode;
+                tail.NextNode = null;
+            }
+
+            Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currNode = head;
+
+            //while (currNode != null)
+            //{
+            //    action(currNode.Value);
+            //    currNode = currNode.NextNode;
+            //}
+
+            for (int i = 1; i <= Count; i++)
+            {
+                action(currNode.Value);
+                currNode = currNode.NextNode;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+
+            var currNode = head;
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = currNode.Value;
+                currNode = currNode.NextNode;
+            }
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            ListNode currNode = this.head;
+
+            while (currNode != null)
+            {
+                yield return currNode.Value;
+                currNode = currNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/08.CustomLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using CustomDoublyLinkedList;
+using System;
+
+namespace _08.CustomLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<int> doubly = new DoublyLinkedList<int>();
+
+            doubly.AddFirst(5);
+            doubly.AddFirst(4);
+            doubly.AddFirst(3);
+
+            doubly.AddLast(5);
+            doubly.AddLast(4);
+            doubly.AddLast(3);
+
+            foreach (var item in doubly)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
