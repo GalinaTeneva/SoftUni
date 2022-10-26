07ef31763ca3ce9ac95a59a848ebb9c47feed188
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-26 16:28:23 +0300

    Added new problem
    Added problem "07.CustomComparator"  in folder "Exercise" in "10.IteratorsAndComparators"

---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/07.CustomComparator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> customComparator = (x, y) =>
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+
+                if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return x.CompareTo(y);
+            };
+
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Array.Sort(nums, (x, y) => customComparator(x, y));
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
