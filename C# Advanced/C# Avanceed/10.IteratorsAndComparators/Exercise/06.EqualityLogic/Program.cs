a17dbebff08c9206a28d2ce5ccbb44c08de59fd0
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-26 15:34:46 +0300

    Added new problem
    Added problem "06.EqualityLogic" in folder "Exercise" in solution "10.IteratorsAndComparators"

---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
