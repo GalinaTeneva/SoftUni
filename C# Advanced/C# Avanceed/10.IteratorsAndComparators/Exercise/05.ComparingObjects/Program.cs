ef63e83f605294f67c7bdeb331199038cbb8fd5b
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-26 11:58:15 +0300

    Branch (#1)
    * Changing files names
    
    * Added new problem's solution
    
    Added problem "05.ComparingObjects" to folder "Exercise" in solution "10.IteratorsAndComparators"

---
+++ C# Advanced/C# Avanceed/10.IteratorsAndComparators/Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string cmd = Console.ReadLine();
+
+            while (cmd != "END")
+            {
+                string[] personInfo = cmd.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+                people.Add(person);
+
+                cmd = Console.ReadLine();
+            }
+
+            int index = int.Parse(Console.ReadLine());
+            Person personToCompare = people[index - 1];
+
+            int matchesCount = 0;
+            int diffCount = 0;
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(personToCompare) == 0)
+                {
+                    matchesCount++;
+                }
+                else
+                {
+                    diffCount++;
+                }
+            }
+
+            if (matchesCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matchesCount} {number of not equal people} {total number of people}");
+            }
+        }
+    }
+}
