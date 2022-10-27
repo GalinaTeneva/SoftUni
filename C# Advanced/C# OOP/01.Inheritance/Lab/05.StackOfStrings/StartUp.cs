973f2da7c1a6945d82a1157b0c8bb7c7087014f9
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-27 16:22:38 +0300

    Added new problem
    Added lab problem "05.StackOfStrings" in solution "01.Inheritance" in folder "OOP"

---
+++ C# Advanced/C# OOP/01.Inheritance/Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.AddRange(new List<string>() { "1", "2", "3" });
+            stack.AddRange(new List<string>() { "1", "2", "3" });
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
