debd22e6a056cde5beca547d320e9b53f8aa1ac2
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-27 19:57:13 +0300

    Added new problem
    Added problem "02.RecursiveFactorial" in solution "11.BasicAlgorithms", part of folder "C#Advanced"

---
+++ C# Advanced/C# Avanceed/11.BasicAlgorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(num));
+        }
+
+        static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
