63fb31e25cf4ff76109db9f6712d2f8b7db9652f
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-10-27 16:07:12 +0300

    Added new problem
    Added lab problem "04.RandomList" in solution "01.Inheritance" in "OOP" folder

---
+++ C# Advanced/C# OOP/01.Inheritance/Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("6");
+            list.Add("8");
+            list.Add("10");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
