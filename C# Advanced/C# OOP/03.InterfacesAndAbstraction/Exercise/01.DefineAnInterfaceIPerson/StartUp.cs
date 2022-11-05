ed22953535287f59160f74409099f0d87e4df03e
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-05 10:12:15 +0200

    refactoring

--- C# Advanced/C# OOP/03.InterfacesAndAbstraction/Exercise/01.DefineAnInterfaceIPerson/StartUp.cs
+++ C# Advanced/C# OOP/03.InterfacesAndAbstraction/Exercise/01.DefineAnInterfaceIPerson/StartUp.cs
@@ -8,7 +8,9 @@ namespace PersonInfo
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
+
             IPerson person = new Citizen(name, age);
+
             Console.WriteLine(person.Name);
             Console.WriteLine(person.Age);
 
