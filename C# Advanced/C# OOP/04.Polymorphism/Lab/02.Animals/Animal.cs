01582e10df690d707b8d3638fe17a1bc0c0a2f1d
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-08 00:36:30 +0200

    Added new problem
    Added lab problem "02.Animals" in solution "04.Polymorphism", part of "OOP"

---
+++ C# Advanced/C# OOP/04.Polymorphism/Lab/02.Animals/Animal.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string FavouriteFood
+        {
+            get { return favouriteFood; }
+            set { favouriteFood = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
