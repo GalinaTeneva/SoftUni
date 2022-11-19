88963f96330370f1131459285dd8b13edb5244be
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-19 11:36:21 +0200

    Added new problem
    Added new lab problem "02.EnterNumbers" in solution "05.ExceptionsAndErrorHandling", part of "OOP".

---
+++ C# Advanced/C# OOP/05.ExceptionsAndErrorHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>();
+
+            int currNum = 1;
+            int endNum = 100;
+            while (numbers.Count != 10)
+            {
+                try
+                {
+                    currNum = ReadNumber(currNum, endNum);
+                    numbers.Add(currNum);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+
+            int num;
+            if (!int.TryParse(input, out num))
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+
+            return num;
+        }
+    }
+}
