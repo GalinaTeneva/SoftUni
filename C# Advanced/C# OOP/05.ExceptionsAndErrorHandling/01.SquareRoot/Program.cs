70a578a34131c36dae79d1be09bb33d56629dbcf
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-19 10:54:17 +0200

    Added new problem
    Added new lab problem "01.SquareRoot" in solution "05.ExceptionsAndErrorHandling", part of "OOP"

---
+++ C# Advanced/C# OOP/05.ExceptionsAndErrorHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                CalculateSquareRoot(num);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        static void CalculateSquareRoot(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            Console.WriteLine(Math.Sqrt(num));
+        }
+    }
+}
