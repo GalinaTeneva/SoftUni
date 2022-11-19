4546ad3757a0cb114eab633dcdd6b95c07d91a46
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-19 15:13:34 +0200

    Added new problem
    Added new lab problem "04.SumOfIntegers" in solution "05.ExceptionsAndErrorHandling", part of "OOP".

---
+++ C# Advanced/C# OOP/05.ExceptionsAndErrorHandling/04.SumOfIntegers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SumOfIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int integerSum = 0;
+
+            foreach (string item in input)
+            {
+                try
+                {
+                    int num = ItemAsInteger(item);
+                    integerSum += num;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                }
+                catch (OverflowException oe)
+                {
+                    Console.WriteLine(oe.Message);
+                }
+
+                Console.WriteLine($"Element '{item}' processed - current sum: {integerSum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {integerSum}");
+        }
+
+        static int ItemAsInteger(string input)
+        {
+            long longNum = 0;
+            int num = 0;
+
+            if (Int64.TryParse(input, out longNum))
+            {
+                if (longNum > int.MaxValue || longNum < int.MinValue)
+                {
+                    throw new OverflowException($"The element '{input}' is out of range!");
+                }
+                if (!int.TryParse(input, out num))
+                {
+                    throw new FormatException($"The element '{input}' is in wrong format!");
+                }
+            }
+
+            return num;
+        }
+    }
+}
+
