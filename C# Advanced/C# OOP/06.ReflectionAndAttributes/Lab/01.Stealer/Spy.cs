5cf48d535d87b9c9433816a9c55f5b654a224d68
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-22 15:55:59 +0200

    Added new problem
    Added new lab problem "01.Stealer" in solution "06.ReflectionAndAttributes", part of "OOP".

---
+++ C# Advanced/C# OOP/06.ReflectionAndAttributes/Lab/01.Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            Type classType = Type.GetType(className);
+
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (FieldInfo field in classFields.Where(f => fieldsNames.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
