d4ac5a185a4295631263cbb712912c874690de39
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2022-11-26 15:12:45 +0200

    Update

--- C# Advanced/C# OOP/06.ReflectionAndAttributes/Exercise/02.ValidationAttributes/Utilities/Validator.cs
+++ C# Advanced/C# OOP/06.ReflectionAndAttributes/Exercise/02.ValidationAttributes/Utilities/Validator.cs
@@ -2,11 +2,8 @@
 namespace ValidationAttributes.Utilities
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using System.Text;
-    using System.Threading.Tasks;
     using ValidationAttributes.Utilities.Attributes;
 
     public static class Validator
