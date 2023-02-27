87566c409205cc3ad9ea0907bbd1369a7270180d
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-02-27 13:46:12 +0200

    Added new method
    Added method "GetEmployeesWithSalaryOver50000"

--- C# DB/ENTITY FRAMEWORK CORE/03.EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ C# DB/ENTITY FRAMEWORK CORE/03.EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -13,8 +13,13 @@ public class StartUp
         SoftUniContext dbContext = new SoftUniContext();
 
         //03. Employees Full Information
-        string result = GetEmployeesFullInformation(dbContext);
-        Console.WriteLine(result);
+        string result1 = GetEmployeesFullInformation(dbContext);
+        Console.WriteLine(result1);
+
+        //04. Employees with Salary Over 50 000
+        string result2 = GetEmployeesWithSalaryOver50000(dbContext);
+        Console.WriteLine(result2);
+
     }
 
     //03. Employees Full Information
@@ -37,6 +42,29 @@ public class StartUp
         foreach (var employee in employees)
         {
             sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //04. Employees with Salary Over 50 000
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Salary > 50_000)
+            .OrderBy(e => e.FirstName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
         }
 
         return sb.ToString().TrimEnd();
