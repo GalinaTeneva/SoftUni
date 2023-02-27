6b3a5b403722423b0d82cab60a5a71c840d1bda9
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-02-27 13:24:10 +0200

    Added new method
    Added method "GetEmployeesFullInformation"

--- C# DB/ENTITY FRAMEWORK CORE/03.EntityFrameworkIntroduction/SoftUni/StartUp.cs
+++ C# DB/ENTITY FRAMEWORK CORE/03.EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
 
 namespace SoftUni;
 
@@ -9,8 +11,34 @@ public class StartUp
     static void Main(string[] args)
     {
         SoftUniContext dbContext = new SoftUniContext();
-        Console.WriteLine("Connected!");
+
+        //03. Employees Full Information
+        string result = GetEmployeesFullInformation(dbContext);
+        Console.WriteLine(result);
     }
 
+    //03. Employees Full Information
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
 
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
 }
