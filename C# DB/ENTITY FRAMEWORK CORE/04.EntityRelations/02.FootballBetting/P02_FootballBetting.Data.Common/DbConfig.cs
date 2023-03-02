92f717a98ed70d52d53cf857ef0e642598b75ef9
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-02 23:19:51 +0200

    Added new sln and problem
    Added problem "FootballBetting" in solution "04.EntitiRelations"

---
+++ C# DB/ENTITY FRAMEWORK CORE/04.EntityRelations/02.FootballBetting/P02_FootballBetting.Data.Common/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace P02_FootballBetting.Data.Common;
+
+public static class DbConfig
+{
+    public const string ConnectionString =
+        @"Server=GALYA-PC\SQLEXPRESS;Database=FootballBetting;Integrated Security=True;Encrypt=False;";
+}
---
+++ C# DB/ENTITY FRAMEWORK CORE/04.EntityRelations/02.FootballBetting/P02_FootballBetting.Data.Models/Enums/Prediction.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace P02_FootballBetting.Data.Models.Enums;
+
+public enum Prediction
+{
+    Win = 1,
+    Lose = 2,
+    Draw = 0
+}
