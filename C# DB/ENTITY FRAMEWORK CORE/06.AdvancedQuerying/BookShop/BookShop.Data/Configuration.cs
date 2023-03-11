afd6881cad8e5e2b098e3d7fd8dc26890bd6bd6d
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-11 16:30:54 +0200

    Creating new DB "BookShop"

---
+++ C# DB/ENTITY FRAMEWORK CORE/06.AdvancedQuerying/BookShop/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=GALYA-PC\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
