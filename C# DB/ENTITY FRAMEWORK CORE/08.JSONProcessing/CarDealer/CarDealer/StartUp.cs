732c00e6d34e406eb52c8ada39e3d0bf4ca89c5d
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-22 10:21:45 +0200

    Added new methods
    - added method "ImportSuppliers"
    -Added method "CreateMapper"

--- C# DB/ENTITY FRAMEWORK CORE/08.JSONProcessing/CarDealer/CarDealer/StartUp.cs
+++ C# DB/ENTITY FRAMEWORK CORE/08.JSONProcessing/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,45 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
 
+            //01. Import Suppliers
+            string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            string result = ImportSuppliers(context, inputJson);
+
+            Console.WriteLine(result);
+        }
+
+        //01. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            ImportSupplierDto[] supplierDtos = JsonConvert.DeserializeObject<ImportSupplierDto[]>(inputJson);
+
+            Supplier[] suppliers = mapper.Map<Supplier[]>(supplierDtos);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}.";
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
         }
     }
 }
