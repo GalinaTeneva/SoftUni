089c0207e46a47aaf014a82f17205710856b1465
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-24 11:37:09 +0200

    Added new method
    - added method "ImportSuppliers"

--- C# DB/ENTITY FRAMEWORK CORE/09.XMLProcessing/CarDealer/CarDealer/StartUp.cs
+++ C# DB/ENTITY FRAMEWORK CORE/09.XMLProcessing/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,67 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using CarDealerContext context = new CarDealerContext();
 
+            //09. Import Suppliers
+            string inputXml = File.ReadAllText(@"../../../Datasets.suppliers.xml");
+            string result = ImportSuppliers(context, inputXml);
+
+            Console.WriteLine(result);
+        }
+
+        //09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (ImportSupplierDto supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                //Manual mapping
+                //Supplier supplier = new Supplier()
+                //{
+                //    Name = supplierDto.Name,
+                //    IsImporter = supplierDto.IsImporter
+                //};
+
+                IMapper mapper = CreateMapper();
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+
+                validSuppliers.Add(supplier);
+
+            }
+
+            context.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
         }
     }
 }
---
+++ C# DB/ENTITY FRAMEWORK CORE/09.XMLProcessing/CarDealer/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,31 @@
+﻿using CarDealer.DTOs.Import;
+using System.Xml.Serialization;
+
+namespace CarDealer.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T deserializedObject = (T)xmlSerializer.Deserialize(reader);
+
+            return deserializedObject;
+        }
+
+        public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T[] deserializedCollection = (T[])xmlSerializer.Deserialize(reader);
+
+            return deserializedCollection;
+        }
+    }
+}
