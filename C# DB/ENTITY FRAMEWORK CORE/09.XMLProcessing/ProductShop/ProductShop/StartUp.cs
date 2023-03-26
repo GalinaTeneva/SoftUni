0395f591a9621edb81632c43a781164574bf9b91
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-26 17:58:10 +0300

    Uploading skeleton

---
+++ C# DB/ENTITY FRAMEWORK CORE/09.XMLProcessing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //01.Import Users
+            ProductShopContext context = new ProductShopContext();
+
+            string inputXml = File.ReadAllText("../../../Datasets/users.xml");
+            string result = ImportUsers(context, inputXml);
+
+            Console.WriteLine(result);
+        }
+
+        //01.Import Users
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+            IMapper mapper = CreateMapper();
+
+            ImportUserDto[] userDtos = xmlHelper.Deserialize<ImportUserDto[]>(inputXml, "Users");
+
+            ICollection<User> validUsers = new HashSet<User>();
+            foreach (ImportUserDto userDto in userDtos)
+            {
+                if (string.IsNullOrEmpty(userDto.FirstName) ||
+                    string.IsNullOrEmpty(userDto.LastName))
+                {
+                    continue;
+                }
+
+                User user = mapper.Map<User>(userDto);
+                validUsers.Add(user);
+            }
+
+            context.Users.AddRange(validUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validUsers.Count}";
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+    }
+}
---
+++ C# DB/ENTITY FRAMEWORK CORE/09.XMLProcessing/ProductShop/ProductShop/Utilities/XmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T deserializedObject = (T)xmlSerializer.Deserialize(reader);
+
+            return deserializedObject;
+        }
+    }
+}
