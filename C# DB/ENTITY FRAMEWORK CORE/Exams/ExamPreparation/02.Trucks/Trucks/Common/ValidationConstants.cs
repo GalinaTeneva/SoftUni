35cddd38b043b7b51e9d7e469654d268e0678156
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-03-29 13:37:25 +0300

    Exam "Trucks" initial upload
    - solution skeleton
    - Model Definition

---
+++ C# DB/ENTITY FRAMEWORK CORE/Exams/ExamPreparation/02.Trucks/Trucks/Common/ValidationConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Trucks.Common
+{
+    public static class ValidationConstants
+    {
+        //Client
+        public const int ClientNameMaxLength = 40;
+        public const int ClientNameMinLength = 3;
+
+        public const int ClientNationalityMaxLength = 40;
+        public const int ClientNationalityMinLength = 2;
+
+        //Despatcher
+        public const int DespatcherNameMaxLength = 40;
+        public const int DespatcherMinLength = 2;
+
+        //Truck
+        public const int TruckRegistrationNumberLength = 8;
+        public const int TruckVinNumberLength = 17;
+
+    }
+}
---
+++ C# DB/ENTITY FRAMEWORK CORE/Exams/ExamPreparation/02.Trucks/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Common;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        public Despatcher()
+        {
+            this.Trucks = new HashSet<Truck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.DespatcherNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public string? Position { get; set; }
+
+        public ICollection<Truck> Trucks { get; set; }
+    }
+}
