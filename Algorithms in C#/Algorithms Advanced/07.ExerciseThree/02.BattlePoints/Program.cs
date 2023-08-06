e65d9f4067ab3fee431eb6ac36d9bce8d0ec6b15
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2023-08-06 22:34:41 +0300

    Added new problem
    Added problem "02. Battle Points" in solution "ExerciseThree"

---
+++ Algorithms in C#/Algorithms Advanced/07.ExerciseThree/02.BattlePoints/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BattlePoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] energyCost = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] battlePoints = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int initialEnergy = int.Parse(Console.ReadLine());
+
+            int[,] dp = new int[energyCost.Length + 1, initialEnergy + 1];
+            bool[,] isSpend = new bool[energyCost.Length + 1, initialEnergy + 1];
+
+            for (int row = 1; row < dp.GetLength(0); row++)
+            {
+                int enemyIdx = row - 1;
+
+                for (int energy = 1; energy < dp.GetLength(1); energy++)
+                {
+                    int excluding = dp[row - 1, energy];
+
+                    if (energyCost[enemyIdx] > energy)
+                    {
+                        dp[row, energy] = excluding;
+                        continue;
+                    }
+
+                    int including = battlePoints[enemyIdx] + dp[row - 1, energy - energyCost[enemyIdx]];
+
+                    if (including > excluding)
+                    {
+                        dp[row, energy] = including;
+                        isSpend[row, energy] = true;
+                    }
+                    else
+                    {
+                        dp[row, energy] = excluding;
+                    }
+                }
+            }
+
+            int currEnergy = initialEnergy;
+            int totalPoints = 0;
+
+            for (int row = dp.GetLength(0) - 1; row > 0; row--)
+            {
+                int enemyIdx = row - 1;
+
+                if (!isSpend[row, currEnergy])
+                {
+                    continue;
+                }
+
+                totalPoints += battlePoints[enemyIdx];
+                currEnergy -= energyCost[enemyIdx];
+
+                if (currEnergy == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(totalPoints);
+        }
+    }
+}
